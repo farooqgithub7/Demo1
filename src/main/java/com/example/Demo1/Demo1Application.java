78d302c5bf7b9790bf7e34497b89a242274177ec
farooqgithub7 <shaikfarooqm1777@gmail.com>
2024-12-13 18:18:21 +0530

    [10001]: added the statements

--- src/main/java/com/example/Demo1/Demo1Application.java
+++ src/main/java/com/example/Demo1/Demo1Application.java
@@ -9,8 +9,12 @@ public class Demo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 
-
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+
 
 	}
 
